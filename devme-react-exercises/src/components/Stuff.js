366424cfc88705801d602a50da0b678003b070fb
Nik Osvalds <nosvalds@gmail.com>
2020-05-25 15:11:22 +0100

    02 Props - add square prop to stuff

--- devme-react-exercises/src/components/Stuff.js
+++ devme-react-exercises/src/components/Stuff.js
@@ -4,16 +4,20 @@ import Paragraph from './Paragraph';
 import Square from './Square';
 import People from './People';
 
-const Stuff = () => {
+const Stuff = ({ square }) => {
     let names = ["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"];
   return (
     <React.Fragment>
         <Header text="Hello, world" />
         <Paragraph message="lorem" />
-        <Square />
+        { square ? <Square /> : null}
         <People names={names} />
     </React.Fragment>
   );
+}
+
+Stuff.defaultProps = {
+    square: true
 }
 
 export default Stuff;
