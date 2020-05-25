813b42cb5e40b6a721ea5e96afc8095286937b21
Nik Osvalds <nosvalds@gmail.com>
2020-05-25 12:48:25 +0100

    01 JSX - Square component complete

--- devme-react-exercises/src/App.js
+++ devme-react-exercises/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Paragraph from './components/Paragraph';
+import Square from './components/Square';
 
 function App() {
   return (
     <>
     <Header />
     <Paragraph />
+    <Square />
     </>
   );
 }
---
+++ devme-react-exercises/src/components/Square.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Square = () => {
+    return (
+        <div style={{
+            width: "200px",
+            height: "200px",
+            backgroundColor: "salmon"
+             }}>
+        </div>
+    )
+}
+
+
+export default Square;
