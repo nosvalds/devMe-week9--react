fb7974b7b15c8260a5deb080f78c3c4aad79d35f
Nik Osvalds <nosvalds@gmail.com>
2020-05-27 17:36:22 +0100

    05 Forms - URLs for components with props

--- devme-react-exercises/src/App.js
+++ devme-react-exercises/src/App.js
@@ -6,7 +6,9 @@ import Header from './components/Header';
 import Clicked from './components/state/Clicked';
 import Length from './components/forms/Length';
 import Adder from './components/forms/Adder';
-
+import ToggleText from './components/state/ToggleText';
+import LameGame from './components/tricksy_state/LameGame';
+import Forms from './components/forms/Forms'
 
 const App = () => (
     <Router>
@@ -16,10 +18,31 @@ const App = () => (
             <Link to="/clicked">Clicked</Link>
             <Link to="/length">Length</Link>
             <Link to="/adder">Adder</Link>
+            <Link to="/toggle-text">Toggle Text</Link>
+            <Link to="/lame-game">Lame Game</Link>
+            <Link to="/forms">Forms</Link>
         </nav>
         <Route exact path="/clicked" component={Clicked} />
         <Route exact path="/length" component={Length} />
         <Route exact path="/adder" component={Adder} />
+        <Route path="/toggle-text">
+            <ToggleText 
+            initial="Hello" 
+            alternate="World"
+            />
+        </Route>
+        <Route path="/lame-game">
+            <LameGame
+                aim={ 5 }
+            />
+        </Route>
+        <Route path="/forms">
+            <Forms 
+            fields={
+                        ["First Name", "Last Name", "Emails", "Job Titles"]
+                    } 
+            />
+        </Route>
     </Router>
 )
 
