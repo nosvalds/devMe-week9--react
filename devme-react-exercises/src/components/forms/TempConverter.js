0a76fe7592a8e7a7738bacb3dbbc49a625add10b
Nik Osvalds <nosvalds@gmail.com>
2020-05-27 13:00:34 +0100

    04 Forms - TempConverter with 2 handler methods

--- devme-react-exercises/src/components/forms/TempConverter.js
+++ devme-react-exercises/src/components/forms/TempConverter.js
@@ -4,38 +4,46 @@ class TempConverter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tempC: 0
+            tempC: 0,
+            tempF: 32
         }
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeC = this.handleChangeC.bind(this);
+        this.handleChangeF = this.handleChangeF.bind(this);
     }
 
-    handleChange(e) {
-        if (e.currentTarget.matches("#C")) {
-            this.setState({ tempC: e.currentTarget.value });
-        } else {
-            let tempC = (e.currentTarget.value - 32) * (5 / 9);
-            this.setState({ tempC: tempC });
-        }
+    handleChangeF(e) {
+        let tempF = e.currentTarget.value;
+        let tempC = (tempF - 32) * (5 / 9);
+        this.setState({
+            tempC: tempC,
+            tempF: tempF
+        })
+    }
+
+    handleChangeC(e) {
+        let tempC = e.currentTarget.value;
+        let tempF = tempC * (9 / 5) + 32;
+        this.setState({
+            tempC: tempC,
+            tempF: tempF
+        })
     }
 
     render() {
-        const { tempC } = this.state;
-        let tempF = tempC * (9 / 5) + 32;
+        const { tempC, tempF } = this.state;
         return (
             <>
                 <label>°C</label>
                 <input 
                     type="text" 
-                    id="C"
-                    onChange={(e) => this.handleChange(e)}
+                    onChange={(e) => this.handleChangeC(e)}
                     value={ tempC }
                 >
                 </input>
                 <label>°F</label>
                 <input 
                     type="text"
-                    id="F" 
-                    onChange={(e) => this.handleChange(e)}
+                    onChange={(e) => this.handleChangeF(e)}
                     value={ tempF }
                 >
                 </input>
