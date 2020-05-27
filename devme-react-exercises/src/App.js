196c8668992548cba2c31c9d92ab65edf873c1ae
Nik Osvalds <nosvalds@gmail.com>
2020-05-27 17:30:43 +0100

    05 Router - URLs no props

--- devme-react-exercises/src/App.js
+++ devme-react-exercises/src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './App.css';
-import Stuff from './components/Stuff';
-import FadeIn from './components/FadeIn';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-const App = () => (<FadeIn time="5000ms"><Stuff /></FadeIn>)
+import Header from './components/Header';
+import Clicked from './components/state/Clicked';
+import Length from './components/forms/Length';
+import Adder from './components/forms/Adder';
+
+
+const App = () => (
+    <Router>
+        <Header>Welcome to my App</Header>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/clicked">Clicked</Link>
+            <Link to="/length">Length</Link>
+            <Link to="/adder">Adder</Link>
+        </nav>
+        <Route exact path="/clicked" component={Clicked} />
+        <Route exact path="/length" component={Length} />
+        <Route exact path="/adder" component={Adder} />
+    </Router>
+)
 
 export default App;
---
+++ devme-react-exercises/src/components/FourOhFour.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const FourOhFour = () => (
+    <>
+        <h3>That ain't right: 404</h3>
+        <h3>You must be lost</h3>
+        <Link to="/"></Link>
+    </>
+)
+
+export default FourOhFour;
