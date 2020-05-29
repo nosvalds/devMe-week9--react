1307f5f91040c51820b6fcecf50a81630339f59c
Nik Osvalds <nosvalds@gmail.com>
2020-05-29 14:14:25 +0100

    tried to loop the footer, commented out

--- devme-react-exercises/src/components/Footer.js
+++ devme-react-exercises/src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Footer = () => (
+const Footer = () => {
+    const links = {
+        "/": "Home",
+        "/all-stuff": "Everything",
+    }
+    let key = 0;
+    return (
     <footer>
-        <Link to="/">Home</Link>
+        {/* { links.forEach((name, url) => {
+            key += 1;
+            return (<Link key={ key } to={ url }>{ name }</Link>)
+        })} */}
         <Link to="/all-stuff">Everything</Link>
         <Link to="/clicked">Clicked</Link>
         <Link to="/length">Length</Link>
@@ -12,6 +21,7 @@ const Footer = () => (
         <Link to="/lame-game">Lame Game</Link>
         <Link to="/forms">Forms</Link>
     </footer>
-)
+    )
+}
 
 export default Footer;
