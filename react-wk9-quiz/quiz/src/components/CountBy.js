b9f7b7ef27b51eb418e782bc0202ef477362c803
Nik Osvalds <nosvalds@gmail.com>
2020-05-29 16:02:52 +0100

    Quiz 03 - Count with bootstrap complete

--- react-wk9-quiz/quiz/src/components/CountBy.js
+++ react-wk9-quiz/quiz/src/components/CountBy.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card'
 
 class CountBy extends Component {
 
@@ -20,24 +20,14 @@ class CountBy extends Component {
     render() {
         const { clicked } = this.state;
         return (
-            // <Card className="text-center" style={{ width: '18rem' }}>
-            //     <Card.Body>
-            //         <Card.Title>Quiz 02 - EvenClicks</Card.Title>
-            //         <Card.Text onClick={ this.handleClick }>
-            //             { display }
-            //         </Card.Text>
-            //     </Card.Body>
-            // </Card>
-            <>
-                <h3>
-                    Quiz 03 - Count By
-                </h3>
-                <p 
-                    onClick={ this.handleClick }
-                >
-                    { clicked }
-                </p>
-            </>
+            <Card className="text-center" style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Quiz 03 - Count By</Card.Title>
+                    <Card.Text onClick={ this.handleClick }>
+                        { clicked }
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         );
     }
 }
